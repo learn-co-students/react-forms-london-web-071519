43711f5abb77fa6cc388ab57db92fafe700adf16
Harry Chopra <harrychopra@gmail.com>
2019-09-21 21:48:17 +0100

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,38 @@
 // Code ControlledInput Component Here
+import React, {Component} from 'react';
+
+export default class ControlledInput extends Component {
+  state = {
+    firstName: 'Harry',
+    secondName: 'Chopra'
+  };
+
+  handleSubmit = () => {};
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.secondName}
+            onChange={this.handleChange}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+}
